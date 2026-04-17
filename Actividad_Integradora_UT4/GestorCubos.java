0a6ead140f66c6ea6f3a58aa90ee84ac7bf19287
Alvaro A. Mora Acosta <alvaroandresmoraacosta95@gmail.com>
2026-04-17 01:00:48 +0000

    Modifique la clase GestorCubos para añadir los comentarios de JavaDoc que se me olvidaron.

--- Actividad_Integradora_UT4/GestorCubos.java
+++ Actividad_Integradora_UT4/GestorCubos.java
@@ -3,16 +3,38 @@ package Actividad_Integradora_UT4;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Esta clase se encarga de gestionar una lista de cubos, tiene como atributo privado la propia lista de cubos,
+ * un constructor vacio que inicializa la lista de cubos, un metodo getter para poder acceder a la lista de cubos desde otra clase,
+ * un metodo que inicializa limpia y pobla la lista con los cubos introducidos por el usuario,
+ * otro metodo que ordena la lista de cubos por su lado de mayor a menor, y por ultimo un metodo
+ * que recibe dos cubos y devuelve un booleano basandose en si caben uno dentro del otro o no.
+ */
+
 public class GestorCubos {
     private ArrayList<Cubo> listaCubos;
+
+    /**
+     * Constructor vacio que inicializa la lista de cubos.
+     */
 
     public GestorCubos() {
         listaCubos = new ArrayList<Cubo>();
     }
 
+    /**
+     * Metodo getter para poder acceder a la lista de cubos desde otra clase.
+     * @return
+     */
+
     public ArrayList<Cubo> getListaCubos() {
         return listaCubos;
     }
+
+    /**
+     * Metodo que inicializa limpia y pobla la lista con los cubos introducidos por el usuario.
+     * @param cubosIniciales
+     */
 
     public void inicializarYPoblar(ArrayList<Cubo> cubosIniciales) {
         listaCubos.clear();
@@ -21,9 +43,21 @@ public class GestorCubos {
         }
     }
 
+    /**
+     * Metodo que ordena la lista de cubos por su lado de mayor a menor.
+     */
+
     public void ordenarCubosporLadoMayorMenor() {
         Collections.sort(listaCubos, Collections.reverseOrder());
     }
+
+    /**
+     * Metodo que recibe dos cubos y devuelve un booleano basandose en si caben uno dentro del otro o no. Gracias a su tamaño de lado, si el lado del cubo interno es menor al externo
+     * por logica cabra dentro.
+     * @param interno
+     * @param externo
+     * @return
+     */
 
     public boolean cabeDentro(Cubo interno, Cubo externo) {
         if(interno.getLado() < externo.getLado()) {
